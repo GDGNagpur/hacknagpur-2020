2a8f762a91230a6ec4521384253450a43f6ce662
Tushar Choudhari <choudharit23@gmail.com>
2020-11-05 17:37:57 +0530

    :lipstick: Sass support added

--- components/ComingSoon.js
+++ components/ComingSoon.js
@@ -1,5 +1,20 @@
 const ComingSoon = () => {
-  return <div>Coming soon</div>;
+  return (
+    <div className='coming-soon'>
+      <div className='logo-big'>
+        <span className='green'>hack</span>nagpur
+        <span className='green'>.</span>
+      </div>
+      <h5 className='tagline'>
+        Central India's largest community{' '}
+        <span className='hackathon'>
+          <span className='green'>&#60;&#60;</span>HACKATHON
+          <span className='green'>&#62;&#62;</span>
+        </span>
+        .
+      </h5>
+    </div>
+  );
 };
 
 export default ComingSoon;
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,22 @@
 import Head from 'next/head';
 import ComingSoon from '../components/ComingSoon';
-import styles from '../styles/Home.module.scss';
+import Header from '../components/Header';
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <Head>
-        <meta charset='UTF-8' />
-        <meta name='viewport' content='width=device-width, initial-scale=1.0' />
-        <title>HackNagpur - Coming Soon</title>
-      </Head>
-      <ComingSoon />
+    <div className='container'>
+      <Header />
+      <div className='main'>
+        <Head>
+          <meta charset='UTF-8' />
+          <meta
+            name='viewport'
+            content='width=device-width, initial-scale=1.0'
+          />
+          <title>HackNagpur - Coming Soon</title>
+        </Head>
+        <ComingSoon />
+      </div>
     </div>
   );
 }

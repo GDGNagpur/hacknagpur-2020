5f45e045d7cab586e901744adf9e7b5ca36f4d8e
Tushar Choudhari <choudharit23@gmail.com>
2020-11-08 18:09:51 +0530

    :art: Minimal changes

--- components/ComingSoon.js
+++ components/ComingSoon.js
@@ -1,22 +1,31 @@
 const ComingSoon = () => {
   return (
     <div className='coming-soon'>
-      <div className='logo-big'>
-        <span className='green'>hack</span>nagpur
-        <span className='green'>.</span>
+      <div>
+        <div className='logo-big'>
+          <span className='green'>hack</span>nagpur
+          <span className='green'>.</span>
+        </div>
+        <h5 className='tagline'>
+          Central India's largest community{' '}
+          <span className='hackathon'>
+            <span className='green'>&#60;&#60;</span>HACKATHON
+            <span className='green'>&#62;&#62;</span>
+          </span>
+          .
+        </h5>
+        <h4 className='coming-soon-text '>Coming Soon...</h4>
+        <a
+          className='subscribe'
+          href='http://eepurl.com/hdvNmn'
+          target='_blank'
+        >
+          Subscribe for updates
+        </a>
       </div>
-      <h5 className='tagline'>
-        Central India's largest community{' '}
-        <span className='hackathon'>
-          <span className='green'>&#60;&#60;</span>HACKATHON
-          <span className='green'>&#62;&#62;</span>
-        </span>
-        .
-      </h5>
-      <h4 className='coming-soon-text '>Coming Soon...</h4>
-      <a className='subscribe' href='http://eepurl.com/hdvNmn' target='_blank'>
-        Subscribe for updates
-      </a>
+      <div>
+
+      </div>
     </div>
   );
 };

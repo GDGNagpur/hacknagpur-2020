4286d207cc49370188b8292f807bfbc6fb17bae3
Tushar Choudhari <choudharit23@gmail.com>
2020-11-13 22:34:55 +0530

    :sparkles: Multiple Sections added

--- components/About.jsx
+++ components/About.jsx
@@ -2,13 +2,15 @@ import about from '../data/about';
 
 const About = () => {
   return (
-    <div id='about'>
+    <div id='about' className='section'>
       <h2 className='section-heading'>
         About<span className='green'>.</span>
       </h2>
-      {about.map((paragraph, index) => (
-        <p key={index}>{paragraph}</p>
-      ))}
+      <div className='section-wrapper'>
+        {about.map((paragraph, index) => (
+          <p key={index}>{paragraph}</p>
+        ))}
+      </div>
     </div>
   );
 };
--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,32 @@
 import Head from 'next/head';
+import SketchWrapper from '../components/Sketch';
 import About from '../components/About';
 import Hero from '../components/Hero';
-import SketchWrapper from '../components/Sketch';
+import Tracks from '../components/Tracks';
+import FAQs from '../components/FAQs';
 
 export default function Home() {
   return (
-    <div className='container'>
-      <div className='main'>
-        <Head>
-          <meta charSet='UTF-8' />
-          <meta
-            name='viewport'
-            content='width=device-width, initial-scale=1.0'
-          />
-          <title>HackNagpur - Coming Soon</title>
-        </Head>
-        <main>
-          <Hero />
-          <About />
-        </main>
+    <>
+      <SketchWrapper />
+      <div className='container'>
+        <div className='main'>
+          <Head>
+            <meta charSet='UTF-8' />
+            <meta
+              name='viewport'
+              content='width=device-width, initial-scale=1.0'
+            />
+            <title>HackNagpur - Coming Soon</title>
+          </Head>
+          <main>
+            <Hero />
+            <About />
+            <Tracks />
+            <FAQs />
+          </main>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
